813f777a8c1ede793d5b74dbba5f123bd45542d5
Bryce Huhtala <sketchius@gmail.com>
2022-03-16 18:11:45 -0400

    Add expressionComponent object prototype and begin implementing syntax logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,21 @@
+
+
+
+function ExpressionComponent( type, value, previousComponent, nextCompenent) {
+    this.type = type;
+    this.value = value;
+    this.previousComponent = previousComponent;
+    this.nextCompenent = nextCompenent;
+    this.priority = 0;
+}
+
+const expressionComponents = [];
+
+
+
+
+
+
 let leftOperand = {
     id: 'LEFT',
     text: '0'
@@ -36,24 +54,83 @@ document.addEventListener('keydown',(e) => {
 
 
 function processInput ( inputType, inputCode ) {
+    const currentExpressionComponent = expressionComponents[expressionComponents.length-1];
+
+    debugger;
+
+    let action = '';
+
     switch (inputType) {
-        case 'NUM':
-            processNumberInput( inputCode );
+        case 'Number':
+            if (inputCode.includes('.'))
+                action = getNewComponentAction(currentExpressionComponent,inputType,inputCode,'ExclusiveAppend');
+            else
+                action = getNewComponentAction(currentExpressionComponent,inputType,inputCode,'Append');
             break;
 
-        case 'OPR':
-            processOperatorInput( inputCode );
+        case 'Operator':
+            action = getNewComponentAction(currentExpressionComponent,inputType,inputCode,'Overwrite');
             break;
 
-        case 'SPL':
-            processSpecialInput( inputCode );
+        case 'Special':
+            action = getNewComponentAction(currentExpressionComponent,inputType,inputCode,'ExclusiveAppend');
             break;
     }
+
+    switch ( action ) {
+        case 'Create':
+            expressionComponents.push(new ExpressionComponent(inputType,inputCode,currentExpressionComponent===undefined?null:currentExpressionComponent,null));
+            break;
+        case 'Append':
+            currentExpressionComponent.value = currentExpressionComponent.value + inputCode;
+            break;
+        case 'Overwrite':
+            currentExpressionComponent.type = inputType;
+            currentExpressionComponent.value = inputCode;
+    }
+
+    console.table(expressionComponents);
 
     updateDisplay();
 }
 
-function processNumberInput ( input ) {
+function getNewComponentAction (currentExpressionComponent, inputType, input, sameTypeRule ) {
+    if (currentExpressionComponent === undefined) return 'Create';
+
+    if (currentExpressionComponent.type == inputType) {
+        switch (sameTypeRule) {
+            case 'Append':
+                return 'Append';
+            case 'ExclusiveAppend':
+                if (!currentExpressionComponent.value.includes(input))
+                    return 'Append';
+                else
+                    return 'NullAction';
+            case 'Overwrite':
+                return 'Overwrite';
+        }
+    } else {
+        return 'Create';
+    }
+    
+}
+
+function processNumberInput ( currentExpressionComponent, input ) {
+
+    switch ( getNewComponentAction(currentExpressionComponent,'Number',input,'Append') ) {
+        case 'Create':
+            expressionComponents.push(new ExpressionComponent('Number',input,null,null));
+            break;
+        case 'Append':
+            currentExpressionComponent.value = currentExpressionComponent.value + input;
+            break;
+        case 'Overwrite':
+            currentExpressionComponent.type = 'Number';
+            currentExpressionComponent.value = input;
+    }
+
+    console.table(expressionComponents);
+
     if (currentObject.id == 'OPERATOR') {
         currentObject = rightOperand;
     }
@@ -63,7 +140,22 @@ function processNumberInput ( input ) {
     currentObject.text = currentObject.text + input;
 }
 
-function processOperatorInput ( input ) {
+function processOperatorInput ( currentExpressionComponent, input ) {
+    debugger;
+    if (currentExpressionComponent === undefined) {
+        expressionComponents.push(new ExpressionComponent('Operator',input,null,null));
+    } else {
+        switch (currentExpressionComponent.type) {
+            case 'Number':
+                expressionComponents.push(new ExpressionComponent('Operator',input,null,null));
+                break;
+        }
+    }
+
+
+    console.table(expressionComponents);
+
+
     operator.text = input;
     switch (currentObject.id) {
         case 'LEFT':
@@ -71,7 +163,7 @@ function processOperatorInput ( input ) {
             currentObject = operator;
             break;
         case 'RIGHT':
-            //This means the user entered something like "4 + 2 +". Meaning that we need to process the current operation
+            //This means the user entered something like '4 + 2 +'. Meaning that we need to process the current operation
             //first, then copy the result to the left operand and retain the new operator that was just inputed.
             runEquals();
             currentObject = operator;
@@ -79,18 +171,35 @@ function processOperatorInput ( input ) {
     }
 }
 
-function processSpecialInput ( input ) {
+function processSpecialInput ( currentExpressionComponent, input ) {
 
 
     switch ( input ) {
         case 'EQL':
-            if (currentObject.id == 'RIGHT' && rightOperand.text != "") {
+            if (currentObject.id == 'RIGHT' && rightOperand.text != '') {
                 runEquals();
                 operator.text = '';
                 currentObject = leftOperand;
             }
             break;
-        case 'DOT':
+        case '.':
+            if (currentExpressionComponent === undefined) {
+                expressionComponents.push(new ExpressionComponent('Number','0'+input,null,null));
+            } else {
+                switch (currentExpressionComponent.type) {
+                    case 'Number':
+                        if (!currentExpressionComponent.value.includes('.')) {
+                            currentExpressionComponent.value = currentExpressionComponent.value + input;
+                        }
+                        break;
+                    case 'Operator':       
+                        expressionComponents.push(new ExpressionComponent('Number','0'+input,null,null));   
+                        break;
+                }
+            }
+
+
+
             if (currentObject.id != 'OPERATOR') {
                 if (!currentObject.text.includes('.')) {
                     currentObject.text = currentObject.text + '.';
@@ -111,9 +220,9 @@ function processSpecialInput ( input ) {
             break;
         case 'CLR':
             currentObject = leftOperand;
-            leftOperand.text = "0";
-            rightOperand.text = "";
-            operator.text = "";
+            leftOperand.text = '0';
+            rightOperand.text = '';
+            operator.text = '';
             break;
         case 'BCK':
             if (currentObject.id != 'OPERATOR') {
@@ -122,6 +231,7 @@ function processSpecialInput ( input ) {
             break;
     }
 
+    console.table(expressionComponents);
     
 }
 
@@ -154,7 +264,7 @@ function performCalculation( leftComponent, rightComponent, operator ) {
 function processDecimal ( number ) {
     // This limits the number of decimal points based on how many digits
     // are being used on the left side of the decimal.
-    let currentDigits = (parseInt(number)+"").length;
+    let currentDigits = (parseInt(number)+'').length;
     // Below, the '+' in '+number.toFixed(...' will remove any unnecessary decimal digits as
     // part of the conversion from string (i.e. '10.0000000000000' -> 10) 
     return +number.toFixed(Math.max(0,15-currentDigits));
